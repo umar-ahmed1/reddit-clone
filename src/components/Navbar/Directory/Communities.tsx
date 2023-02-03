2fe17cf825468a6da0b9a3ce2c6b97ab17271996
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-02-02 22:56:36 -0700

    .

---
+++ src/components/Navbar/Directory/Communities.tsx
@@ -0,0 +1,34 @@
+import { MenuItem, Flex, Icon } from '@chakra-ui/react';
+import React from 'react';
+import CreateCommunityModal from '../../Modal/CreateCommunity/CreateCommunityModal';
+import {GrAdd} from 'react-icons/gr'
+type CommunitiesProps = {
+    
+};
+
+const Communities:React.FC<CommunitiesProps> = () => {
+    const [open,setOpen] = React.useState(false)
+    return (
+        <>
+            <CreateCommunityModal
+                open={open}
+                handleClose={()=>setOpen(false)}
+            />
+            <MenuItem 
+                width='100%' 
+                fontSize='10pt' 
+                _hover={{bg:'gray.100'}} 
+                onClick={()=>setOpen(true)}
+            >
+                <Flex align='center'>
+                    <Icon fontSize={20} mr={2} as={GrAdd}/>
+                    Create Community
+                </Flex>
+            </MenuItem>
+        
+        </>
+    
+    
+    )
+}
+export default Communities;
--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { Flex, Icon, Tooltip} from '@chakra-ui/react';
 import React from 'react';
 import {BsArrowUpRightCircle, BsChatDots} from 'react-icons/bs'
 import{GrAdd} from 'react-icons/gr' 
@@ -21,9 +21,12 @@ const Icons:React.FC<IconsProps> = () => {
     return (
         <Flex>
             <Flex display ={{base:'none',md:'flex'}} align='center' borderRight={2} borderColor='gray.200'>
-                <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
-                    <Icon as={BsArrowUpRightCircle} fontSize={20}/>
-                </Flex>
+                <Tooltip hasArrow label='Popular' fontSize='9pt'>
+                    <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
+                        <Icon as={BsArrowUpRightCircle} fontSize={20}/>
+                    </Flex>
+                </Tooltip>
+                
                 <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
                     <Icon as={IoFilterCircleOutline} fontSize={22}/>
                 </Flex>
@@ -32,15 +35,21 @@ const Icons:React.FC<IconsProps> = () => {
                 </Flex>
             </Flex>
             <> 
-                <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
-                    <Icon as={BsChatDots} fontSize={20}/>
-                </Flex>
-                <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
-                    <Icon as={IoNotificationsOutline} fontSize={20}/>
-                </Flex>
-                <Flex display={{base:'none', md:'flex'}} mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
-                    <Icon as={GrAdd} fontSize={20}/>
-                </Flex>
+                <Tooltip hasArrow label='Chat' fontSize='9pt'>
+                    <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
+                        <Icon as={BsChatDots} fontSize={20}/>
+                    </Flex>
+                </Tooltip>
+                <Tooltip hasArrow label='Notifications' fontSize='9pt'>
+                    <Flex mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
+                        <Icon as={IoNotificationsOutline} fontSize={20}/>
+                    </Flex>
+                </Tooltip>
+                <Tooltip hasArrow label='Create Post' fontSize='9pt'>
+                    <Flex display={{base:'none', md:'flex'}} mr={1.5} ml={1.5} padding={1} cursor="pointer" borderRadius={4} _hover={{bg:'gray.200'}}>
+                        <Icon as={GrAdd} fontSize={20}/>
+                    </Flex>
+                </Tooltip>
             
             </>
         </Flex>
