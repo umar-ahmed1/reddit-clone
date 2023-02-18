5fb8d3738c74be9dc55d8fc175d9e95127dcdcbf
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-02-17 18:52:39 -0700

    feb 17 complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import { useRecoilValue } from "recoil";
 import { communityState } from "../atoms/communitiesAtom";
 import { Post, PostVote } from "../atoms/postsAtom";
 import CreatePostLink from "../components/Community/CreatePostLink";
+import PersonalHome from "../components/Community/PersonalHome";
+import Premium from "../components/Community/Premium";
+import Recommendations from "../components/Community/Recommendations";
 import PageContent from "../components/Layout/PageContent";
 import PostItem from "../components/Posts/PostItem";
 import PostLoader from "../components/Posts/PostLoader";
@@ -135,9 +138,11 @@ export default function Home() {
             </Stack>
         )}
       </>
-      <>
-
-      </>
+      <Stack spacing={5}>
+        <Recommendations/>
+        <Premium/>
+        <PersonalHome/>
+      </Stack>
     </PageContent>
   )
 }
